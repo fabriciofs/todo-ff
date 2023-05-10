47989a545846169e0efcc64a3c6d76a2795b7613
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2023-05-10 15:36:27 -0300

    clear project

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -29,10 +29,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [
-
-      'axios',
-    ],
+    boot: [],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: [
@@ -56,11 +53,11 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -128,7 +125,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -139,7 +136,7 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
         'render' // keep this as last one
--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -2,9 +2,9 @@
   <q-page class="items-center flex column">
     <h2 class="">TODO-LIST</h2>
     <div class="row">
-      <q-input outlined v-model="newTodo" @keyup.enter="addTodo" class="q-mr-sm"/>
+      <q-input outlined v-model="newTodo" @keyup.enter="addTodo" class="q-mr-sm" />
       <div style="max-width: 300px">
-        <q-input filled v-model="proxyDate" class="q-mr-sm">
+        <q-input outlined v-model="proxyDate" class="q-mr-sm">
           <template v-slot:prepend>
             <q-icon name="event" class="cursor-pointer">
               <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -16,7 +16,6 @@
               </q-popup-proxy>
             </q-icon>
           </template>
-
           <template v-slot:append>
             <q-icon name="access_time" class="cursor-pointer">
               <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -30,15 +29,15 @@
           </template>
         </q-input>
       </div>
-      <q-btn :disabled="!proxyDate||!newTodo" color="secondary" size="sm" @click="addTodo">Adicionar</q-btn>
+      <q-btn :disabled="!proxyDate || !newTodo" color="secondary" size="sm" @click="addTodo">Adicionar</q-btn>
     </div>
     <ul style="min-width: 300px">
-      <li v-for="todo in todos" :key="todo.id" @click="increment" class="flex items-center justify-between">
-        <span v-if="!todo.edit" :class="todo.isDone ? 'text-strike' : ''" class="q-mr-sm"> {{ todo.description }} {{ ' || '}} </span>
-         <i v-if="!todo.edit" :class="todo.isDone ? 'text-strike' : ''" > {{ todo.dueDate }} </i>
+      <li v-for="todo in todos" :key="todo.id" class="flex items-center justify-between">
+        <span v-if="!todo.edit" :class="todo.isDone ? 'text-strike' : ''" class="q-mr-sm"> {{ todo.description }} </span>
+        <i v-if="!todo.edit" :class="todo.isDone ? 'text-strike' : ''"> {{ todo.dueDate }} </i>
         <q-input v-if="todo.edit" v-model="todo.description" @keyup.enter="saveTodo(todo)"></q-input>
         <div v-if="todo.edit" style="max-width: 300px">
-          <q-input filled v-model="todo.dueDate" class="q-mr-sm">
+          <q-input outlined v-model="todo.dueDate" class="q-mr-sm">
             <template v-slot:prepend>
               <q-icon name="event" class="cursor-pointer">
                 <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -50,7 +49,6 @@
                 </q-popup-proxy>
               </q-icon>
             </template>
-
             <template v-slot:append>
               <q-icon name="access_time" class="cursor-pointer">
                 <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -65,67 +63,56 @@
           </q-input>
         </div>
         <div class="right">
-          <q-checkbox v-if="!todo.edit" v-model="todo.isDone" @update:model-value="checkTodo(todo, $event)"/>
-          <q-btn v-if="!todo.edit" title="remover item" round icon="remove" color="red" size="xs" @click="removeItem(todo.id)"></q-btn>
-          <q-btn class="q-mx-sm" v-if="!todo.edit" title="remover item" round icon="edit" color="blue" size="xs" @click="editItem(todo)"></q-btn>
-          <q-btn v-if="todo.edit" title="remover item" round icon="save" color="blue" size="xs" @click="saveTodo(todo)"></q-btn>
+          <q-checkbox v-if="!todo.edit" v-model="todo.isDone" @update:model-value="checkTodo(todo, $event)" />
+          <q-btn v-if="!todo.edit" title="remover item" round icon="remove" color="red" size="xs"
+            @click="removeItem(todo.id)"></q-btn>
+          <q-btn class="q-mx-sm" v-if="!todo.edit" title="remover item" round icon="edit" color="blue" size="xs"
+            @click="editItem(todo)"></q-btn>
+          <q-btn v-if="todo.edit" title="remover item" round icon="save" color="blue" size="xs"
+            @click="saveTodo(todo)"></q-btn>
         </div>
       </li>
     </ul>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import {onMounted, ref } from 'vue';
-import { api } from 'boot/axios'
+<script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 
 const newTodo = ref('')
 const proxyDate = ref('')
-const todos = ref<Todo[]>([]);
-
-const meta = ref<Meta>({
-  totalCount: 0
-});
+const todos = ref([]);
 
 const checkTodo = async (item, isDone) => {
   const newPayload = {
     ...item,
     isDone
   }
-  await api.put(`/todos/${item.id}`, newPayload)
+  await axios.put(`http://localhost:3000/todos/${item.id}`, newPayload)
 }
+
 const saveTodo = async (todo) => {
-  await api.put(`/todos/${todo.id}`, todo)
+  await axios.put(`http://localhost:3000/todos/${todo.id}`, todo)
   const index = todos.value.findIndex(item => item.id === todo.id)
   todos.value[index].edit = false
 }
 const removeItem = async (id) => {
-  await api.delete(`/todos/${id}`)
+  await axios.delete(`http://localhost:3000/todos/${id}`)
   await updateTodo()
 }
 
-const editItem = async(todo) => {
+const editItem = async (todo) => {
   const index = todos.value.findIndex(item => item.id === todo.id)
   todos.value[index].edit = true
 }
-// const dateObj = new Date(dateString);
-// const options = {
-//   timeZone: 'America/Sao_Paulo',
-//   year: 'numeric',
-//   month: '2-digit',
-//   day: '2-digit',
-//   hour: '2-digit',
-//   minute: '2-digit',
-//   second: '2-digit',
-//   hour12: false,
-// };
+
 const addTodo = async () => {
   if (!newTodo.value.trim()) return
   const dateString = proxyDate.value;
   const dateObj = new Date(dateString);
   const isoString = dateObj.toISOString();
-  await api.post('/todos', {
+  await axios.post('http://localhost:3000/todos', {
     description: newTodo.value,
     dueDate: isoString,
     isDone: false
@@ -136,10 +123,9 @@ const addTodo = async () => {
 }
 
 const updateTodo = () => {
-  return api.get('/todos')
+  return axios.get('http://localhost:3000/todos')
     .then((response) => {
       todos.value = response.data
-      meta.value.totalCount = response.data.length
     })
 }
 
